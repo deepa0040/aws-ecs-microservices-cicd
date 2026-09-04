707447f8832b6acc87dfe350d4507257406d6166
deepachand10 <deepa.chand@firminiq.com>
2026-09-04 12:07:18 +0530

    update ci.yaml

--- ECS/child-module/ecr-service/main.tf
+++ ECS/child-module/ecr-service/main.tf
@@ -16,6 +16,6 @@ locals {
 resource "aws_ecr_repository" "repo" {
   for_each             = toset(local.images)
   name                 = "${var.project_name}/${each.value}"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
   force_delete         = true
 }
